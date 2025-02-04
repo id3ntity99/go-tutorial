23a55033367b59e007c390218478610f1479e54d
id3ntity99 <jas06113@gmail.com>
2025-02-04 13:54:11 +0900

    Learned value and pointer receiver

--- receiver/main.go
+++ receiver/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 
-	"github.com/id3ntity99/module/entity"
+	person "github.com/id3ntity99/module/entity"
 )
 
 func main() {
-	person := person.NewPerson("John Doe", "Male", 25)
-	x := person.GetOlderUsingPointerRecv(10)
-	y := person.GetOlderUsingValueRecv(10)
-	fmt.Println(x)
-	fmt.Println(y)
+	john := person.NewPerson("John Doe", "Male", 25)
+
+	fmt.Printf("[Info] John is %d years old now\n", john.GetAge())
+	ages := john.GetOlderUsingValueRecv(10)
+	fmt.Printf("[Info] Using value receiver, John gets old to age of %d years old\n", ages)
+	fmt.Printf("[GetAge()] John is %d years old now\n", john.GetAge())
+
+	ages = john.GetOlderUsingPointerRecv(10)
+	fmt.Printf("[Info] Using pointer receiver, John gets old to age of %d years old\n", ages)
+	fmt.Printf("[GetAge()] John is %d years old now\n", john.GetAge())
 }
