c6943913ed1c63d2648f4da8de3df72c69aa42a2
id3ntity99 <jas06113@gmail.com>
2025-02-05 18:02:14 +0900

    Routing for resources url

--- networking/main.go
+++ networking/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/id3ntity99/go/networking/basics"
+	"github.com/id3ntity99/go/networking/htmlfinder"
+)
+
+func main() {
+	http.HandleFunc("/hello", basics.HelloHandler)
+	http.HandleFunc("/hello/world", basics.HelloHandler)
+	http.HandleFunc("/welcome", basics.WelcomeHandler)
+
+	http.HandleFunc("/files/", htmlfinder.ResourceHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
