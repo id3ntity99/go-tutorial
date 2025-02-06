03d6d4f3a8bf675ada643cb93f504f7f55a5543b
id3ntity99 <jas06113@gmail.com>
2025-02-06 19:18:05 +0900

    Use helper and resource handler

--- networking/main.go
+++ networking/main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/id3ntity99/go/networking/basics"
 	"github.com/id3ntity99/go/networking/htmlfinder"
 )
 
+// log.fatal kept calling exit(1), which was constantly shutting down my program
 func main() {
-	http.HandleFunc("/hello", basics.HelloHandler)
-	http.HandleFunc("/hello/world", basics.HelloHandler)
-	http.HandleFunc("/welcome", basics.WelcomeHandler)
+	wlog := *log.New(os.Stdout, "[Warning]: ", log.LstdFlags)
+	wd, err := os.Getwd()
 
-	http.HandleFunc("/files/", htmlfinder.ResourceHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	if err != nil {
+		wlog.Println("Failed while getting currently working directory")
+	}
+
+	h := htmlfinder.NewHandler(wd)
+
+	htmlfinder.Make(h.HandleFetchResource)
+
+	http.HandleFunc("/resources/", htmlfinder.Make(h.HandleFetchResource))
+	http.ListenAndServe(":8080", nil)
 }
