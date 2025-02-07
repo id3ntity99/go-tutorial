46e6a92723cd2550302cc9da636a6f4b0824daed
id3ntity99 <jas06113@gmail.com>
2025-02-07 17:20:27 +0900

    Use HandleHTML, which serves specific HTML file

--- networking/htmlfinder/resource.go
+++ networking/htmlfinder/resource.go
@@ -8,40 +8,34 @@ import (
 )
 
 type FetchResourceHandler struct {
-	path       string
+	wd         string
 	infoLogger *log.Logger
 }
 
 // Constructor
-func NewHandler(path string) *FetchResourceHandler {
+func NewHandler(wd string) *FetchResourceHandler {
 	return &FetchResourceHandler{
 		infoLogger: log.New(os.Stdout, "[INFO] ", log.LstdFlags),
-		path:       path,
+		wd:         wd,
 	}
 }
 
-func (handler *FetchResourceHandler) HandleFetchResource(w http.ResponseWriter, r *http.Request) error {
-	sepstr := string(os.PathSeparator)
-	target := handler.path + strings.ReplaceAll(r.URL.Path, "/", sepstr)
-	//check if the requested file exists
-	stat, err := os.Stat(target)
+// This function returns URI for target file
+func (h *FetchResourceHandler) toURL(r *http.Request) string {
+	//FIXME Return value has duplicate of "/"
+	fname := r.URL.Query().Get("name")
+	sep := string(os.PathSeparator)
+	uri := strings.Replace(r.URL.Path, "/", "", 1)
+	uri = strings.ReplaceAll(uri, "/", sep)
+	return h.wd + sep + uri + sep + fname
+}
 
-	if err != nil { // If error occurs
-		return err
-	} else {
-		//Log stat.Name()
-		handler.infoLogger.Printf("Requested resource %s has found\n", stat.Name())
-	}
-
-	//if the file exists, write it into response body and send it to the client and return nil
-	file, err := os.ReadFile(target)
-
-	if err != nil {
-		return err
-	}
-
-	w.Header().Set("Content-type", "text/html;charset=utf-8")
-	w.Write(file)
-
+func (h *FetchResourceHandler) HandleHTML(w http.ResponseWriter, r *http.Request) error {
+	// 1. Extract file name from query string
+	uri := h.toURL(r)
+	// 3. stat() the target file
+	w.Write([]byte("<h3>" + "uri: " + uri + "<h3>"))
+	// 4. if it exists, read the file, else return error
+	// 5. Write the file into response body
 	return nil
 }
