2dd8606bbf7ab8c619ee0f2bb2034011726c260d
id3ntity99 <jas06113@gmail.com>
2025-02-07 17:19:43 +0900

    Changed error page tags

--- networking/htmlfinder/helper.go
+++ networking/htmlfinder/helper.go
@@ -18,7 +18,7 @@ func Make(f APIFunc) http.HandlerFunc {
 		if err != nil {
 			switch e := err.(type) {
 			case (*os.PathError):
-				msg := fmt.Sprintf("<h1 style=\"color: red\"> %s does not exist.</h1><h3>stack trace </br>%s</h3>", e.Path, e.Error())
+				msg := fmt.Sprintf("<h1 style=\"color: red\">404 - Not Found</h1><p>stack trace </br>%s</p>", e.Error())
 				w.WriteHeader(http.StatusNotFound)
 				w.Header().Set("Content-type", "text/html;charset=utf-8")
 				w.Write([]byte(msg))
